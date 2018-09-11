2a69b096ed1a2b62860518ed6ca193cad98077ff
cpele <geekarist@users.noreply.github.com>
2018-09-11 08:44:40 +0200

    Persist AuthState

--- app/src/main/java/me/cpele/myappauthsample/MainActivity.kt
+++ app/src/main/java/me/cpele/myappauthsample/MainActivity.kt
@@ -5,8 +5,8 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import net.openid.appauth.*
 import java.io.InputStreamReader
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private companion object {
         private const val CLIENT_ID = "511828570984-fuprh0cm7665emlne3rnf9pk34kkn86s.apps.googleusercontent.com"
+        private const val PREF_KEY_AUTH_STATE = "AUTH_STATE"
     }
 
     private lateinit var authorizationService: AuthorizationService
@@ -31,36 +32,59 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+
+        intent?.apply {
+            val jsonAuthState = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                    .getString(PREF_KEY_AUTH_STATE, null)
+            if (jsonAuthState != null) {
+                val authState = AuthState.jsonDeserialize(jsonAuthState)
+                authState.callUserInfo()
+            } else {
+                requestTokenThenCallUserInfo()
+            }
+        }
+    }
+
+    private fun requestTokenThenCallUserInfo() {
         val response = AuthorizationResponse.fromIntent(intent)
         val error = AuthorizationException.fromIntent(intent)
 
         response?.apply {
-            Log.d(localClassName, jsonSerializeString())
             val authState = AuthState(response, error)
             val request = createTokenExchangeRequest()
             authorizationService.performTokenRequest(request) { response, tokenException ->
                 authState.apply {
                     update(response, tokenException)
+                    PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                            .edit()
+                            .putString(PREF_KEY_AUTH_STATE, jsonSerializeString())
+                            .apply()
                 }.apply {
-                    performActionWithFreshTokens(authorizationService) { accessToken, idToken, _ ->
-
-                        Thread {
-                            val url = "https://www.googleapis.com/oauth2/v3/userinfo"
-                            val connection = URL(url).openConnection()
-                            connection.setRequestProperty("Authorization", "Bearer $accessToken")
-                            val inputStream = connection.getInputStream()
-                            val inputStreamReader = InputStreamReader(inputStream)
-                            val strResponse = inputStreamReader.readText()
-                            inputStreamReader.close()
-
-                            AlertDialog.Builder(this@MainActivity)
-                                    .setMessage("Here is your response: $strResponse")
-                                    .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
-                                    .show()
-                        }.start()
-                    }
+                    callUserInfo()
                 }
             }
+        }
+    }
+
+    private fun AuthState.callUserInfo() {
+        performActionWithFreshTokens(authorizationService) { accessToken, idToken, _ ->
+
+            Thread {
+                val url = "https://www.googleapis.com/oauth2/v3/userinfo"
+                val connection = URL(url).openConnection()
+                connection.setRequestProperty("Authorization", "Bearer $accessToken")
+                val inputStream = connection.getInputStream()
+                val inputStreamReader = InputStreamReader(inputStream)
+                val strResponse = inputStreamReader.readText()
+                inputStreamReader.close()
+
+                runOnUiThread {
+                    AlertDialog.Builder(this@MainActivity)
+                            .setMessage("Here is your response: $strResponse")
+                            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+                            .show()
+                }
+            }.start()
         }
     }
 
